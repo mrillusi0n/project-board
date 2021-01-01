1f20b6174a92fbb1fa52012dc2e2e9fa2e556440
Nikhil CSB <csbnikhil@gmail.com>
2021-01-01 23:10:23 +0530

    instantiate admin

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_security import Security
 from flask_login import LoginManager
+from flask_admin import Admin
 
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 security = Security()
+admin = Admin(name='Project Board', template_mode='bootstrap3')
